07b9967f8210278d3ccb33c2d62ad923ba75835d
Daniel M Satiro <mateus_satiro@yahoo.com.br>
2021-11-01 17:55:53 -0300

    estiliza produtos da vitrine

--- src/components/Product/index.jsx
+++ src/components/Product/index.jsx
@@ -3,20 +3,21 @@ import "../../components/Product/style.css";
 function Product({ product, handleClick }) {
   return (
     <li className="conteiner_card">
-      <div className="container_img">
+      <div className="conteiner_img">
         <img className="product--img" src={product.img} alt="imagem produto" />
       </div>
-
-      <h3 className="product--name">{product.name}</h3>
-      <p className="product--category">{product.category}</p>
-      <p className="product--price">{product.price}</p>
-      <button
-        className="button--green"
-        id={product.id}
-        onClick={(event) => handleClick(parseInt(event.target.id, 10))}
-      >
-        Adicionar
-      </button>
+      <div className="conteiner_outros">
+        <h3 className="product product--name">{product.name}</h3>
+        <p className="product product--category">{product.category}</p>
+        <p className="product product--price">R$ {product.price.toFixed(2)}</p>
+        <button
+          className="product button--green"
+          id={product.id}
+          onClick={(event) => handleClick(parseInt(event.target.id, 10))}
+        >
+          Adicionar
+        </button>
+      </div>
     </li>
   );
   /* Imagem, nome, categoria, preço e button para adicionar ao carrinho */
